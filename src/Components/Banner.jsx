cacd21fb6a380ad10d8d343e9fa6fe40849dbe11
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-02-28 19:50:12 +0100

    tick function

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,7 +1,51 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
+import HeaderImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Design", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -20,7 +64,11 @@ export const Banner = () => {
             </p>
             <button onClick={() => console.log("connect")}>
               Let's connect
+              <ArrowRightCircle size={25} />
             </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={" "} alt="Header Img" />
           </Col>
         </Row>
       </Container>
