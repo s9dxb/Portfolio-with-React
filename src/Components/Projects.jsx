991c88d61eb876589cc45e2976b95def56f8f652
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-03-04 16:37:14 +0100

    added projectCard cmponent, projects component css

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+// import "animate.css";
+// import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
   const projects = [
@@ -38,39 +45,61 @@ export const Projects = () => {
     <section className="project" id="project">
       <Container>
         <Row>
-          <Col>
+          <Col size={12}>
             <h2>Projects</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-              ad, facere neque doloremque est laborum quod asperiores temporibus
-              necessitatibus perspiciatis pariatur accusamus, nobis nulla nemo
-              exercitationem iste dicta maxime sed.
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
             </p>
+
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
-              <Nav variant="pills" defaultActiveKey="/home">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
                 <Nav.Item>
-                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
                 </Nav.Item>
               </Nav>
-              <Tab.Content>
+              <Tab.Content id="slideInUp">
                 <Tab.Pane eventKey="first">
                   <Row>
                     {projects.map((project, index) => {
-                      return <p>{project.title}</p>;
+                      return <ProjectCard key={index} {...project} />;
                     })}
                   </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cumque quam, quod neque provident velit, rem explicabo
+                    excepturi id illo molestiae blanditiis, eligendi dicta
+                    officiis asperiores delectus quasi inventore debitis quo.
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cumque quam, quod neque provident velit, rem explicabo
+                    excepturi id illo molestiae blanditiis, eligendi dicta
+                    officiis asperiores delectus quasi inventore debitis quo.
+                  </p>
                 </Tab.Pane>
               </Tab.Content>
             </Tab.Container>
           </Col>
         </Row>
       </Container>
+      <img className="background-image-right" src={colorSharp2} />
     </section>
   );
 };
